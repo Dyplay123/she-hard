65cd3b2e49c1170277bf454e7369df3cc94d3eaa
dyplay123 <2535022128@qq.com>
2022-06-24 16:10:22 +0800

    第3次修改

--- she-hard/src/main/java/com/atguigu/Controller/TestGit.java
+++ she-hard/src/main/java/com/atguigu/Controller/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("我喜欢你啊");
+        System.out.println("这是第三次修改");
     }
 }
